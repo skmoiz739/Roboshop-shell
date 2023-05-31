9acb5afd549510f230848fd83c200b6c3b8ee571
Moeez Shaik <skmoiz739@gmail.com>
2023-05-31 19:52:40 -0400

    nginx server

--- frontend/frontend.sh
+++ frontend/frontend.sh
@@ -3,9 +3,9 @@ yum install nginx -y &>> /tmp/log.file
 echo -e "\e[33m removing user share file \e[0m"
 rm -rf /usr/share/nginx/html/* &>> /tmp/log.file
 echo -e "\e[33m downloading the required files \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> /tmp/log.file
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>> /tmp/log.file
 echo -e "\e[33m creating app dir and unzipping \e[0m"
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>> /tmp/log.file
 unzip /tmp/frontend.zip &>> /tmp/log.file
 
 #we will give config files here
