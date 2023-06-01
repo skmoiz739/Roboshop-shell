7937102d842f54d2455d8a1fc5edd74ef6e13d05
Moeez Shaik <skmoiz739@gmail.com>
2023-06-01 19:39:02 -0400

    settingup user service file

---
+++ cart/cart.sh
@@ -0,0 +1,22 @@
+echo -e "\e[33m downloading nginx \e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/log.file
+echo -e "\e[33m installing nginx \e[0m"
+yum install nodejs -y &>> /tmp/log.file
+echo -e "\e[33m adding user \e[0m"
+useradd roboshop &>> /tmp/log.file
+echo -e "\e[33m creating dir \e[0m"
+mkdir /app &>> /tmp/log.file
+echo -e "\e[33m download and unzip \e[0m"
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>> /tmp/log.file
+cd /app
+unzip /tmp/cart.zip &>> /tmp/log.file
+echo -e "\e[33m installing dependencies \e[0m"
+cd /app
+npm install &>> /tmp/log.file
+echo -e "\e[33m installing service files \e[0m"
+cd /home/centos/Roboshop-shell/cart/cart.service /ect/systemd/system/cart.service &>> /tmp/log.file
+echo -e "\e[33m starting nginx \e[0m"
+systemctl daemon-reload &>> /tmp/log.file
+systemctl enable cart  &>> /tmp/log.file
+systemctl start cart &>> /tmp/log.file
+
--- user/user.sh
+++ user/user.sh
@@ -8,9 +8,9 @@ echo -e "\e[33m creating dir  \e[0m"
 mkdir /app &>> /tmp/log.file
 echo -e "\e[33m downlaoding dependencies and unzipping \e[0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>> /tmp/log.file
-cd /app &>> /tmp/log.file
+cd /app
 unzip /tmp/user.zip &>> /tmp/log.file
-cd /app &>> /tmp/log.file
+cd /app
 npm install &>> /tmp/log.file
 echo -e "\e[33m settingup service files \e[0m"
 cp /home/centos/Roboshop-shell/user/user.service /etc/systemd/system/user.service &>> /tmp/log.file
