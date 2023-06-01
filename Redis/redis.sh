721661fc77a1684b7a54ae2557f5acd634f7b85c
Moeez Shaik <skmoiz739@gmail.com>
2023-06-01 18:45:47 -0400

    setting catalogue

--- Redis/redis.sh
+++ Redis/redis.sh
@@ -5,7 +5,7 @@ yum module enable redis:remi-6.2 -y &>> /tmp/log.file
 echo -e "\e[33m installing redis server \e[0m"
 yum install redis -y &>> /tmp/log.file
 echo -e "\e[33m updating ip adress \e[0m"
-sed -i "s/127.0.0.1/0.0.0.0" /etc/redis.conf &>> /tmp/log.file
+sed -i "s/127.0.0.1/0.0.0.0" /etc/redis.conf /etc/redis/redis.conf &>> /tmp/log.file
 echo -e "\e[33m starting redis server \e[0m"
 systemctl enable redis &>> /tmp/log.file
 systemctl start redis &>> /tmp/log.file
