684ca2dab40e6c681990f9d8f8d2d38b039e0b18
Moeez Shaik <skmoiz739@gmail.com>
2023-06-03 05:02:20 -0400

    settingup vairables

--- catalogue/catalogue.sh
+++ catalogue/catalogue.sh
@@ -8,21 +8,21 @@ yum install nodejs -y &>> ${log_file}
 echo -e "${color} adding user ${nocolor}"
 useradd roboshop &>> ${log_file}
 echo -e "${color} creating dir ${nocolor}"
-mkdir /app &>> ${log_file}
+mkdir ${app_path} &>> ${log_file}
 echo -e "${color} downloading ${component} files ${nocolor}"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>> ${log_file}
 echo -e "${color} unzip ${nocolor}"
-cd /app &>> ${log_file}
+cd ${app_path} &>> ${log_file}
 unzip /tmp/${component}.zip &>> ${log_file}
 echo -e "${color} downloading dependencies ${nocolor}"
-cd /app &>> ${log_file}
+cd ${app_path} &>> ${log_file}
 npm install &>> ${log_file}
 echo -e "${color} system files ${nocolor}"
 cp /home/centos/Roboshop-shell/${component}/${component}.service /etc/systemd/system/${component}.service &>> ${log_file}
 echo -e "${color} schemas ${nocolor}"
 cp /home/centos/Roboshop-shell//mongodb/mongodb.repo /etc/yum.repos.d/mongodb.repo &>> ${log_file}
 yum install mongodb-org-shell -y &>> ${log_file}
-mongo --host mongodb-dev.devops-learning.site </app/schema/${component}.js &>> ${log_file}
+mongo --host mongodb-dev.devops-learning.site <${app_path}/schema/${component}.js &>> ${log_file}
 echo -e "${color} starting ${component} ${nocolor}"
 systemctl daemon-reload &>> ${log_file}
 systemctl enable ${component} &>> ${log_file}
--- common.sh
+++ common.sh
@@ -1,3 +1,4 @@
 color="\e[33m"
 nocolr="\e[0m"
 log_file="/tmp/log.file"
+app_path="/app"
