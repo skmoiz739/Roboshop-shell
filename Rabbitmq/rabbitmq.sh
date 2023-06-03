9bfc31def31a232ac49eabbde65e1e1b33bec43e
Moeez Shaik <skmoiz739@gmail.com>
2023-06-03 03:46:58 -0400

    settingup shipping

--- Rabbitmq/rabbitmq.sh
+++ Rabbitmq/rabbitmq.sh
@@ -1,11 +1,11 @@
 echo -e "\e[33m downloading vendor package \e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>> /tmp/log/file
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>> /tmp/log/file
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>> /tmp/log.file
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>> /tmp/log.file
 echo -e "\e[33m installing rabbitmq server \e[0m"
-yum install rabbitmq-server -y  &>> /tmp/log/file
+yum install rabbitmq-server -y  &>> /tmp/log.file
 echo -e "\e[33m start and enable server \e[0m"
-systemctl enable rabbitmq-server  &>> /tmp/log/file
-systemctl start rabbitmq-server  &>> /tmp/log/file
+systemctl enable rabbitmq-server  &>> /tmp/log.file
+systemctl start rabbitmq-server  &>> /tmp/log.file
 echo -e "\e[33m adding user nd password \e[0m"
-rabbitmqctl add_user roboshop roboshop123  &>> /tmp/log/file
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>> /tmp/log/file
+rabbitmqctl add_user roboshop roboshop123  &>> /tmp/log.file
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>> /tmp/log.file
