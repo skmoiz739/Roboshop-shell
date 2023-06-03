024af08b2c1558cc645b361b2d591e240fb73041
Moeez Shaik <skmoiz739@gmail.com>
2023-06-03 05:05:50 -0400

    settingup vairables

--- cart/cart.sh
+++ cart/cart.sh
@@ -1,22 +1,24 @@
-echo -e "\e[33m downloading nginx \e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/log.file
-echo -e "\e[33m installing nginx \e[0m"
-yum install nodejs -y &>> /tmp/log.file
-echo -e "\e[33m adding user \e[0m"
-useradd roboshop &>> /tmp/log.file
-echo -e "\e[33m creating dir \e[0m"
-mkdir /app &>> /tmp/log.file
-echo -e "\e[33m download and unzip \e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>> /tmp/log.file
-cd /app
-unzip /tmp/cart.zip &>> /tmp/log.file
-echo -e "\e[33m installing dependencies \e[0m"
-cd /app
-npm install &>> /tmp/log.file
-echo -e "\e[33m installing service files \e[0m"
-cd /home/centos/Roboshop-shell/cart/cart.service  /ect/systemd/system/cart.service &>> /tmp/log.file
-echo -e "\e[33m starting nginx \e[0m"
-systemctl daemon-reload &>> /tmp/log.file
-systemctl enable cart  &>> /tmp/log.file
-systemctl start cart &>> /tmp/log.file
+component=cart
+source common.sh
+echo -e "${color} downloading nginx ${nocolor}"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> ${log_file}
+echo -e "${color} installing nginx ${nocolor}"
+yum install nodejs -y &>> ${log_file}
+echo -e "${color} adding user ${nocolor}"
+useradd roboshop &>> ${log_file}
+echo -e "${color} creating dir ${nocolor}"
+mkdir ${app_path} &>> ${log_file}
+echo -e "${color} download and unzip ${nocolor}"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>> ${log_file}
+cd ${app_path}
+unzip /tmp/${component}.zip &>> ${log_file}
+echo -e "${color} installing dependencies ${nocolor}"
+cd ${app_path}
+npm install &>> ${log_file}
+echo -e "${color} installing service files ${nocolor}"
+cd /home/centos/Roboshop-shell/${component}/${component}.service  /ect/systemd/system/${component}.service &>> ${log_file}
+echo -e "${color} starting nginx ${nocolor}"
+systemctl daemon-reload &>> ${log_file}
+systemctl enable ${component}  &>> ${log_file}
+systemctl start ${component} &>> ${log_file}
 
