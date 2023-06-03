4aeba1808118a1027886ac0e086d398f0a7cc7d9
Moeez Shaik <skmoiz739@gmail.com>
2023-06-03 04:51:34 -0400

    settingup shipping

--- catalogue/catalogue.sh
+++ catalogue/catalogue.sh
@@ -1,27 +1,29 @@
-echo -e "\e[33m downloading nodejs \e[0m"
+component=catalogue
+source common.sh
+echo -e "${color} downloading nodejs ${nocolor}"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/log.file
-echo -e "\e[33m installing nodejs \e[0m"
+echo -e "${color} installing nodejs ${nocolor}"
 yum install nodejs -y &>> /tmp/log.file
 
-echo -e "\e[33m adding user \e[0m"
+echo -e "${color} adding user ${nocolor}"
 useradd roboshop &>> /tmp/log.file
-echo -e "\e[33m creating dir \e[0m"
+echo -e "${color} creating dir ${nocolor}"
 mkdir /app &>> /tmp/log.file
-echo -e "\e[33m downloading catalogue files \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> /tmp/log.file
-echo -e "\e[33m unzip \e[0m"
+echo -e "${color} downloading ${component} files ${nocolor}"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>> /tmp/log.file
+echo -e "${color} unzip ${nocolor}"
 cd /app &>> /tmp/log.file
-unzip /tmp/catalogue.zip &>> /tmp/log.file
-echo -e "\e[33m downloading dependencies \e[0m"
+unzip /tmp/${component}.zip &>> /tmp/log.file
+echo -e "${color} downloading dependencies ${nocolor}"
 cd /app &>> /tmp/log.file
 npm install &>> /tmp/log.file
-echo -e "\e[33m system files \e[0m"
-cp /home/centos/Roboshop-shell/catalogue/catalogue.service /etc/systemd/system/catalogue.service &>> /tmp/log.file
-echo -e "\e[33m schemas \e[0m"
+echo -e "${color} system files ${nocolor}"
+cp /home/centos/Roboshop-shell/${component}/${component}.service /etc/systemd/system/${component}.service &>> /tmp/log.file
+echo -e "${color} schemas ${nocolor}"
 cp /home/centos/Roboshop-shell//mongodb/mongodb.repo /etc/yum.repos.d/mongodb.repo &>> /tmp/log.file
 yum install mongodb-org-shell -y &>> /tmp/log.file
-mongo --host mongodb-dev.devops-learning.site </app/schema/catalogue.js &>> /tmp/log.file
-echo -e "\e[33m starting catalogue \e[0m"
+mongo --host mongodb-dev.devops-learning.site </app/schema/${component}.js &>> /tmp/log.file
+echo -e "${color} starting ${component} ${nocolor}"
 systemctl daemon-reload &>> /tmp/log.file
-systemctl enable catalogue &>> /tmp/log.file
-systemctl start catalogue &>> /tmp/log.file
+systemctl enable ${component} &>> /tmp/log.file
+systemctl start ${component} &>> /tmp/log.file
---
+++ common.sh
@@ -0,0 +1,2 @@
+\e[33m=color
+\e[0m=nocolr
